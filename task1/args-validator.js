81534fb2193297820d9c9d36d3bd5fbf1ad6fda7
3igrik <3igrik@gmail.com>
2021-05-09 21:22:30 +0600

    feat: add args-validator

---
+++ task1/args-validator.js
@@ -0,0 +1,53 @@
+import { Command } from "commander";
+import fs from "fs";
+import chalk from "chalk";
+
+const commands = new Command;
+  
+commands.option("-s, --shift <number>", "a shift")
+        .option("-a, --action <type>", "an action encode/decode")
+        .option("-i, --input <input-file>", "an input file")
+        .option("-o, --output <output-file>", "an output file")
+        .parse();
+
+const args = commands.opts(),
+      shift = args.shift,
+      action = args.action,
+      inputFilePath = args.input,
+      outputFilePath = args.output;
+
+function errorHandler(err) {
+  if (err) {
+    process.stderr.write(chalk.red.bgBlack(`${err.message}\n`));
+    process.exit(1);
+  }
+}
+
+function validate() {
+  if (!shift) {
+    errorHandler(new Error("Error: missing required argument: --shift/-s"));
+  } else if (isNaN(Number(shift))) {
+    errorHandler(
+      new Error("Error: shift(--shift/-s) must take the value of a number")
+    );
+  }
+
+  if (!action) {
+    errorHandler(new Error("Error: missing required argument: --action/-a"));
+  } else if (action !== "encode" && action !== "decode") {
+    errorHandler(
+      new Error("Error: action(--action/-a) must be 'encode' or 'decode'")
+    );
+  }
+
+  if (inputFilePath) {
+    fs.access(inputFilePath, fs.constants.R_OK, err => errorHandler(err));
+  }
+
+  if (outputFilePath) {
+    fs.access(outputFilePath, fs.constants.W_OK, err => errorHandler(err));
+  } 
+}
+
+export {errorHandler, action, shift, inputFilePath, outputFilePath};
+export default validate;
